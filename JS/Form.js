9ed8aeb7ce1cd80e6c83f72a304d489d51ac6807
Om <omdhingani0@gmail.com>
2022-11-15 23:27:45 +0530

    first commit

---
+++ JS/Form.js
@@ -0,0 +1,14 @@
+// const form1Btn = document.getElementById('f1');
+
+// form1Btn.addEventListener('click',function(){
+//     window.open("/HTML/Form2.html");
+
+// });
+
+// let form2Btn = document.getElementById('f2');
+
+
+// form2Btn.addEventListener('click',function(){
+//     window.open("/HTML/Form3.html");
+
+// });
